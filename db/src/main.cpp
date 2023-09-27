2265cba5b832dcb671703c99742e665be21a78f5
Albrict <karelinaleksj@gmail.com>
2023-09-27 19:33:26 +0200

    Added getTransportYear function

--- db/src/main.cpp
+++ db/src/main.cpp
@@ -4,11 +4,12 @@
 
 int main (int argc, char *argv[]) 
 {
-    const std::string vin = "SAJEA01U13HM54253";
+    const std::string vin = "19VDE1F75FE004339";
     std::setlocale(LC_ALL, "");
     if (VIN::checkVIN(vin)) {
         std::cout << "Success\n";
         std::cout << VIN::getVINCountry(vin) << '\n';
+        std::cout << "Year of vehicle: " << VIN::getTransportYear(vin) << '\n';
     }
     else
         std::cout << "Failure\n";
